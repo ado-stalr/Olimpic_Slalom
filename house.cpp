70037461346247ab0147967fc225c018c3d24564
ado-stalr <71353192+ado-stalr@users.noreply.github.com>
2021-09-30 19:40:12 +0300

    поправил house.cpp в lw2 после сборки

--- house.cpp
+++ house.cpp
@@ -1,23 +1,22 @@
-#include <SFNL/Graphics.hpp>
+#include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 #include <SFML/System.hpp>
-#include <Color.hpp>
 
 int main()
 {
-
 	//walls
 	sf::RectangleShape walls;
-	walls.setSize({ 455, 240 });
-	walls.setPosition({ 55, 300 });
+	walls.setSize({455, 240});
+	walls.setPosition({55, 300});
 	walls.setFillColor(sf::Color(0x4D, 0x2E, 0x0B));
 
 	//door
 	sf::RectangleShape door;
-	door.setSize({ 75, 163 });
-	door.setPosition({ 100, 375 });
+	door.setSize({75, 163});
+	door.setPosition({100, 375});
 	door.setFillColor(sf::Color(0x18, 0x17, 0x17));
 
+	//roof
 	sf::ConvexShape roof;
 	roof.setFillColor(sf::Color(0x5D, 0x1E, 0x17));
 	roof.setPosition(0, 0);
@@ -29,35 +28,35 @@ int main()
 
 	//smoke
 	sf::CircleShape smoke1(25);
-	smoke1.setPosition({ 400, 30 });
+	smoke1.setPosition({400, 30});
 	smoke1.setFillColor(sf::Color(0xBF, 0xBF, 0xBF));
 
 	sf::CircleShape smoke2(24);
-	smoke2.setPosition({ 388, 60 });
+	smoke2.setPosition({388, 60});
 	smoke2.setFillColor(sf::Color(0xBF, 0xBF, 0xBF));
 
 	sf::CircleShape smoke3(18);
-	smoke3.setPosition({ 375, 85 });
+	smoke3.setPosition({375, 85});
 	smoke3.setFillColor(sf::Color(0xBF, 0xBF, 0xBF));
 
 	sf::CircleShape smoke4(17);
-	smoke4.setPosition({ 365, 113 });
+	smoke4.setPosition({365, 113});
 	smoke4.setFillColor(sf::Color(0xBF, 0xBF, 0xBF));
 
-
-	//truba
+	//tube
 	sf::RectangleShape truba1;
-	truba1.setSize({ 60, 40 });
-	truba1.setPosition({ 340, 140 });
+	truba1.setSize({60, 40});
+	truba1.setPosition({340, 140});
 	truba1.setFillColor(sf::Color(0x3B, 0x38, 0x38));
 
 	sf::RectangleShape truba2;
-	truba2.setSize({ 35, 65 });
-	truba2.setPosition({ 355, 180 });
+	truba2.setSize({35, 65});
+	truba2.setPosition({355, 180});
 	truba2.setFillColor(sf::Color(0x3B, 0x38, 0x38));
 
-	sf::RenderWindow window(sf::VideoMode({569, 548}), 'my house');
-	window.clear(White)
+	//rendering
+	sf::RenderWindow window(sf::VideoMode({569, 548}), "my house");
+	window.clear(sf::Color(0xEF, 0xEF, 0xEF));
 	window.draw(walls);
 	window.draw(door);
 	window.draw(roof);
